ef5ff51fd5d01a180027698f170ddca60719b354
andersbss <and.strands@gmail.com>
2020-02-20 13:22:38 +0100

    Added projects

---
+++ Packit.DataAccess/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Packit.DataAccess
+{
+    public class Class1
+    {
+    }
+}
---
+++ Packit.Database.Migrations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Packit.Database.Migrations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
