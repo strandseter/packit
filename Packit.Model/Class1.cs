41f47321af96261665c19847a2831b818ae588c2
andersbss <and.strands@gmail.com>
2020-02-20 13:35:12 +0100

    Added basic Item class

--- Packit.Model/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Packit.Model
-{
-    public class Class1
-    {
-    }
-}
---
+++ Packit.Model/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Packit.Model
+{
+    public class Item
+    {
+        public int ItemId { get; set; }
+        public string Title { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title} {ItemId}";
+        }
+    }
+}
