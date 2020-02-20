2f54fd45444d14c478ee91fc8a2b4b8080a035e6
andersbss <and.strands@gmail.com>
2020-02-20 13:49:50 +0100

    Test

--- Packit.ConsoleApp/Program.cs
+++ Packit.ConsoleApp/Program.cs
@@ -6,7 +6,7 @@ namespace Packit.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello hællæ!");   
         }
     }
 }
