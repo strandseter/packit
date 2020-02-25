a2c6405b2711f60fd1af5f192ba8d4a96251f14d
andersbss <and.strands@gmail.com>
2020-02-25 20:06:14 +0100

    Restructured Items controller

--- Packit.App/Views/ItemsPage.xaml.cs
+++ Packit.App/Views/ItemsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Packit.Model;
 using System;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using Windows.UI.Xaml;
@@ -7,13 +8,13 @@ using Windows.UI.Xaml.Controls;
 using Windows.Web.Http;
 using static Newtonsoft.Json.JsonConvert;
 
-
 namespace Packit.App.Views
 {
     public sealed partial class ItemsPage : Page, INotifyPropertyChanged
     {
-        static readonly Uri itemsUri = new Uri("http://localhost:52286/api/Items");
-        readonly HttpClient _httpClient = new HttpClient();
+        private static readonly Uri itemsUri = new Uri("http://localhost:52286/api/Items");
+        private readonly HttpClient httpClient = new HttpClient();
+        private static ObservableCollection<Item> ItemsList = new ObservableCollection<Item>();
 
         public ItemsPage()
         {
@@ -24,30 +25,54 @@ namespace Packit.App.Views
 
         private void ItemPage_Loaded(object sender, RoutedEventArgs e)
         {
-            LoadItems();
+            InitializeItems();
+            PopulateGridView();
+        }
+
+        private void PopulateGridView()
+        {
+            ItemsAdaptive1.ItemsSource = ItemsList;
+        }
+
+
+        private async void InitializeItems()
+        {
+            var result = await httpClient.GetAsync(itemsUri);
+            var json = await result.Content.ReadAsStringAsync();
+            var items = DeserializeObject<Item[]>(json);
+            foreach (Item item in items)
+                ItemsList.Add(item);
         }
 
         private void AddButton_Click(object sender, RoutedEventArgs e)
         {
+
             var item = new Item() { Title = "Yeet" };
-            PostItem(item);
-            LoadItems();
-        }
-
-        private async void LoadItems()
-        {
-            var result = await _httpClient.GetAsync(itemsUri);
-            var json = await result.Content.ReadAsStringAsync();
-            var items = DeserializeObject<Item[]>(json);
-
-            ItemsAdaptive1.ItemsSource = items;
+            ItemsList.Add(item);
         }
 
         private async void PostItem(Item item)
         {
             var json = SerializeObject(item);
-            _ = await _httpClient.PostAsync(itemsUri, new HttpStringContent(json, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
+
+            try
+            {
+                _ = await httpClient.PostAsync(itemsUri, new HttpStringContent(json, Windows.Storage.Streams.UnicodeEncoding.Utf8, "application/json"));
+            }
+            catch (ArgumentNullException ex)
+            {
+
+            }
+            catch(InvalidOperationException ex)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
         }
+
 
         private void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
         {
