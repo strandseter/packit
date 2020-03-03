c36624096d3c4002c657e4a2f79293c5d86b6768
andersbss <and.strands@gmail.com>
2020-03-03 18:17:10 +0100

    Added API controllers

---
+++ Packit.Database.Api/Controllers/BackpacksController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Packit.DataAccess;
+using Packit.Model;
+
+namespace Packit.Database.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackpacksController : ControllerBase
+    {
+        private readonly PackitContext _context;
+
+        public BackpacksController(PackitContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Backpacks
+        [HttpGet]
+        public IEnumerable<Backpack> GetBackpacks()
+        {
+            return _context.Backpacks;
+        }
+
+        // GET: api/Backpacks/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetBackpack([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var backpack = await _context.Backpacks.FindAsync(id);
+
+            if (backpack == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(backpack);
+        }
+
+        // PUT: api/Backpacks/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBackpack([FromRoute] int id, [FromBody] Backpack backpack)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != backpack.BackpackId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(backpack).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BackpackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Backpacks
+        [HttpPost]
+        public async Task<IActionResult> PostBackpack([FromBody] Backpack backpack)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Backpacks.Add(backpack);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBackpack", new { id = backpack.BackpackId }, backpack);
+        }
+
+        // DELETE: api/Backpacks/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBackpack([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var backpack = await _context.Backpacks.FindAsync(id);
+            if (backpack == null)
+            {
+                return NotFound();
+            }
+
+            _context.Backpacks.Remove(backpack);
+            await _context.SaveChangesAsync();
+
+            return Ok(backpack);
+        }
+
+        private bool BackpackExists(int id)
+        {
+            return _context.Backpacks.Any(e => e.BackpackId == id);
+        }
+    }
+}
---
+++ Packit.Database.Api/Controllers/TripsController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Packit.DataAccess;
+using Packit.Model;
+
+namespace Packit.Database.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private readonly PackitContext _context;
+
+        public TripsController(PackitContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Trips
+        [HttpGet]
+        public IEnumerable<Trip> GetTrips()
+        {
+            return _context.Trips;
+        }
+
+        // GET: api/Trips/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTrip([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var trip = await _context.Trips.FindAsync(id);
+
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(trip);
+        }
+
+        // PUT: api/Trips/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTrip([FromRoute] int id, [FromBody] Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != trip.TripId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(trip).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TripExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Trips
+        [HttpPost]
+        public async Task<IActionResult> PostTrip([FromBody] Trip trip)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Trips.Add(trip);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetTrip", new { id = trip.TripId }, trip);
+        }
+
+        // DELETE: api/Trips/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTrip([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var trip = await _context.Trips.FindAsync(id);
+            if (trip == null)
+            {
+                return NotFound();
+            }
+
+            _context.Trips.Remove(trip);
+            await _context.SaveChangesAsync();
+
+            return Ok(trip);
+        }
+
+        private bool TripExists(int id)
+        {
+            return _context.Trips.Any(e => e.TripId == id);
+        }
+    }
+}
