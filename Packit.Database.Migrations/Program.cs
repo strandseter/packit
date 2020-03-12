3bcfdccdd1b24dfb16835100487fc2e62212cd2e
andersbss <and.strands@gmail.com>
2020-03-12 23:08:00 +0100

    Laptop compatible

--- Packit.Database.Migrations/Program.cs
+++ Packit.Database.Migrations/Program.cs
@@ -9,17 +9,12 @@ namespace Packit.Database.Migrations
     {
         static void Main()
         {
-            
-
-            //var item = new Item() { Title = "Test", Description = "dfdfsddsfsdfsdfsdf", ImageStringName = "sdfdsff", User = user};
-
+   
             using (var db = new PackitContext())
             {
-                //var user = new User() { FirstName = "dfdsf", LastName = "sdsdf", DateOfBirth = new System.DateTime(), Email = "and.strands@gmail.com", HashedPassword = "sddsfsdf" };
+                var user = new User() { FirstName = "dfdsf", LastName = "sdsdf", DateOfBirth = new System.DateTime(), Email = "and.strands@gmail.com", HashedPassword = "sddsfsdf" };
 
-                var user = db.Users.Where(s => s.UserId == 3).First();
-
-                //db.Users.Add(user);
+                db.Users.Add(user);
 
                 db.Items.Add(new Item() { Title = "111", Description = "dfdfsddsfsdfsdfsdf", ImageStringName = "sdfdsff", User = user });
 
