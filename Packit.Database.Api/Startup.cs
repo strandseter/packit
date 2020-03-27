66d3208e5e92482bb6b8f9c65ff7919ec8c3979e
andersbss <and.strands@gmail.com>
2020-03-27 16:20:32 +0100

    .

--- Packit.Database.Api/Startup.cs
+++ Packit.Database.Api/Startup.cs
@@ -41,7 +41,6 @@ namespace Packit.Database.Api
             ConfigureDatabaseConnection(services);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseCors(u => u
