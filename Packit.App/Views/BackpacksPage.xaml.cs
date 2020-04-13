aec61ab9c139c8c0ff9f1725c8ce8e3ecfe607e7
andersbss <and.strands@gmail.com>
2020-04-13 20:39:47 +0200

    Added backpackspage

---
+++ Packit.App/Views/BackpacksPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Packit.App.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace Packit.App.Views
+{
+    public sealed partial class BackpacksPage : Page
+    {
+        public BackpacksViewModel ViewModel { get; } = new BackpacksViewModel();
+
+        public BackpacksPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
