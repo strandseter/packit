28c6d48d85122cbabcf054cfdfc47f86da9ba2ca
andersbss <and.strands@gmail.com>
2020-04-20 10:55:11 +0200

    New versions

--- Packit.Database.Api.Repository/Repository/Classes/BackpackRepository.cs
+++ Packit.Database.Api.Repository/Repository/Classes/BackpackRepository.cs
@@ -1,13 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Packit.DataAccess;
-using Packit.Database.Api.GenericRepository;
+﻿using Packit.DataAccess;
 using Packit.Database.Api.Repository.Generic;
 using Packit.Database.Api.Repository.Interfaces;
 using Packit.Model;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Packit.Database.Api.Repository.Classes
 {
