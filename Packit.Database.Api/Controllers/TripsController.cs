ca49bd1426903100e98ef64413152711787a0736
andersbss <and.strands@gmail.com>
2020-04-20 17:58:21 +0200

    .

--- Packit.Database.Api/Controllers/TripsController.cs
+++ Packit.Database.Api/Controllers/TripsController.cs
@@ -19,47 +19,47 @@ namespace Packit.Database.Api.Controllers
     [ApiController]
     public class TripsController : PackitApiController
     {
-        private readonly ITripRepository _repository;
+        private readonly ITripRepository repository;
 
         public TripsController(PackitContext context, IAuthenticationService authenticationService, IHttpContextAccessor httpContextAccessor, ITripRepository repository)
             :base(context, authenticationService, httpContextAccessor)
         {
-            _repository = repository;
+            this.repository = repository;
         }
 
         // GET: api/Trips
         [HttpGet]
-        public IEnumerable<Trip> GetTrips() => _repository.GetAll(CurrentUserId());
+        public IEnumerable<Trip> GetTrips() => repository.GetAll(CurrentUserId());
 
         // GET: api/Trips/5
         [HttpGet("{id}")]
-        public async Task<IActionResult> GetTrip([FromRoute] int id) => await _repository.GetByIdAsync(id, CurrentUserId());
+        public async Task<IActionResult> GetTrip([FromRoute] int id) => await repository.GetByIdAsync(id, CurrentUserId());
 
         // PUT: api/Trips/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutTrip([FromRoute] int id, [FromBody] Trip trip) => await _repository.UpdateAsync(id, trip, CurrentUserId());
+        public async Task<IActionResult> PutTrip([FromRoute] int id, [FromBody] Trip trip) => await repository.UpdateAsync(id, trip, CurrentUserId());
 
         // POST: api/Trips
         [HttpPost]
-        public async Task<IActionResult> PostTrip([FromBody] Trip trip) => await _repository.CreateAsync(trip, "GetTrip", CurrentUserId());
+        public async Task<IActionResult> PostTrip([FromBody] Trip trip) => await repository.CreateAsync(trip, "GetTrip", CurrentUserId());
 
         // DELETE: api/Trips/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteTrip([FromRoute] int id) => await _repository.DeleteAsync(id, CurrentUserId());
+        public async Task<IActionResult> DeleteTrip([FromRoute] int id) => await repository.DeleteAsync(id, CurrentUserId());
 
         // PUT: api/trips/3/backpacks/4
         [HttpPut]
         [Route("{tripId}/backpacks/{backpackId}/create")]
-        public async Task<IActionResult> PutBackpackToTrip([FromRoute] int tripId, [FromRoute] int backpackId) => await _repository.CreateManyToManyAsync("GetBackpackTrip", backpackId, tripId);
+        public async Task<IActionResult> PutBackpackToTrip([FromRoute] int tripId, [FromRoute] int backpackId) => await repository.CreateManyToManyAsync("GetBackpackTrip", backpackId, tripId);
 
         // DELETE: api/trips/3/backpacks/7/delete
         [HttpDelete]
         [Route("{tripId}/backpacks/{backpackId}/delete")]
-        public async Task<IActionResult> DeleteBackpackFromTrip([FromRoute] int tripId, [FromRoute] int backpackId) => await _repository.DeleteManyToManyAsync(backpackId, tripId);
+        public async Task<IActionResult> DeleteBackpackFromTrip([FromRoute] int tripId, [FromRoute] int backpackId) => await repository.DeleteManyToManyAsync(backpackId, tripId);
 
         // GET: api/trips/4/backpacks
         [HttpGet]
         [Route("{tripId}/backpacks")]
-        public async Task<IActionResult> GetBackpacksInTrip([FromRoute] int tripId) => await _repository.GetManyToManyAsync(tripId);
+        public async Task<IActionResult> GetBackpacksInTrip([FromRoute] int tripId) => await repository.GetManyToManyAsync(tripId);
     }
 }
