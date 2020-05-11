1b5024cc0348b2a650409a2fe30091bd674e1166
andersbss <and.strands@gmail.com>
2020-05-11 12:04:56 +0200

    .

--- Packit.Model/Models/Item.cs
+++ Packit.Model/Models/Item.cs
@@ -7,8 +7,7 @@ namespace Packit.Model
     public class Item : BaseInformation, IDatabase
     {
         public int ItemId { get; set; }
-        public bool IsChecked { get; set; }
-
+        public ICollection<Checked> ItemChecks { get; } = new List<Checked>();
         public virtual ICollection<ItemBackpack> Backpacks { get; } = new List<ItemBackpack>();
 
         public Item() => ImageStringName = $"image{ItemId}";
@@ -20,5 +19,10 @@ namespace Packit.Model
         public override string ToString() => $"{Title} {ItemId}";
 
         public int GetUserId() => UserId;
+        public class Checked
+        {
+            public bool IsChecked { get; set; }
+            public int BackpackId { get; set; }
+        }
     }
 }
