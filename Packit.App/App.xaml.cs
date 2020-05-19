d9136120316f153be898ca0668d365006318c930
andersbss <and.strands@gmail.com>
2020-05-19 18:48:18 +0200

    Login register pages

--- Packit.App/App.xaml.cs
+++ Packit.App/App.xaml.cs
@@ -21,7 +21,7 @@ namespace Packit.App
             InitializeComponent();
 
             // Deferred execution until used. Check https://msdn.microsoft.com/library/dd642331(v=vs.110).aspx for further info on Lazy<T> class.
-            _activationService = new Lazy<ActivationService>(CreateActivationService);
+            _activationService = new Lazy<ActivationService>(CreateActivationService2);
         }
 
         protected override async void OnLaunched(LaunchActivatedEventArgs args)
@@ -40,6 +40,11 @@ namespace Packit.App
         private ActivationService CreateActivationService()
         {
             return new ActivationService(this, typeof(Views.MainPage), new Lazy<UIElement>(CreateShell));
+        }
+
+        private ActivationService CreateActivationService2()
+        {
+            return new ActivationService(this, typeof(Views.LoginPage));
         }
 
         private UIElement CreateShell()
