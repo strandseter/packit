1ee0cc1d62bfb396db7c40d773768b478461af89
andersbss <and.strands@gmail.com>
2020-05-22 21:21:13 +0200

    Revert "."
    This reverts commit df0bc1d2322fb84aa64575c2540cae2874f84fb9.

--- Packit.App/Services/FileService.cs
+++ Packit.App/Services/FileService.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Windows.Graphics.Imaging;
 using Windows.Storage;
 using Windows.Storage.Streams;
 using Windows.UI.Xaml.Media.Imaging;
@@ -42,20 +40,6 @@ namespace Packit.App.Services
                 await bitmapImage.SetSourceAsync(fileStream);
                 return bitmapImage;
             }
-        }
-
-        public static async Task<byte[]> BitmapImageToByteArray(BitmapImage bitmapImage)
-        {
-            byte[] bytes;
-
-            using (var randomAccessStream = await RandomAccessStreamReference.CreateFromUri(bitmapImage.UriSource).OpenReadAsync())
-            {
-                BitmapDecoder decoder = await BitmapDecoder.CreateAsync(randomAccessStream);
-                PixelDataProvider pixelData = await decoder.GetPixelDataAsync();
-
-                bytes = pixelData.DetachPixelData();
-            }
-            return bytes;
         }
     }
 }
