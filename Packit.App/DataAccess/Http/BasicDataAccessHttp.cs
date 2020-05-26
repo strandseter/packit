b304b31dc90cd857ed090d0f831aef6a26971445
andersbss <and.strands@gmail.com>
2020-05-26 19:57:37 +0200

    Regaion and xml documenting testing

--- Packit.App/DataAccess/Http/BasicDataAccessHttp.cs
+++ Packit.App/DataAccess/Http/BasicDataAccessHttp.cs
@@ -1,4 +1,17 @@
-﻿using Newtonsoft.Json;
+﻿// ***********************************************************************
+// Assembly         : Packit.App
+// Author           : ander
+// Created          : 05-15-2020
+//
+// Last Modified By : ander
+// Last Modified On : 05-26-2020
+// ***********************************************************************
+// <copyright file="BasicDataAccessHttp.cs" company="">
+//     Copyright ©  2020
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using Newtonsoft.Json;
 using Packit.App.Services;
 using Packit.Exceptions;
 using Packit.Model.Models;
@@ -10,11 +23,30 @@ using System.Threading.Tasks;
 
 namespace Packit.App.DataAccess
 {
+    /// <summary>
+    /// Class BasicDataAccessHttp.
+    /// Implements the <see cref="Packit.App.DataAccess.IBasicDataAccess{T}" />
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <seealso cref="Packit.App.DataAccess.IBasicDataAccess{T}" />
     public class BasicDataAccessHttp<T> : IBasicDataAccess<T> where T : IDatabase
     {
+        /// <summary>
+        /// The HTTP client
+        /// </summary>
         private readonly HttpClient httpClient = new HttpClient();
+        /// <summary>
+        /// The base URI
+        /// </summary>
         private static readonly Uri baseUri = new Uri($"http://localhost:52286/api/{typeof(T).Name}s");
 
+        /// <summary>
+        /// add as an asynchronous operation.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
+        /// <exception cref="ArgumentNullException">entity</exception>
         public async Task<bool> AddAsync(T entity)
         {
             //This should not be here when the local database is connected
@@ -44,6 +76,13 @@ namespace Packit.App.DataAccess
             return true;
         }
 
+        /// <summary>
+        /// delete as an asynchronous operation.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
+        /// <exception cref="ArgumentNullException">entity</exception>
         public async Task<bool> DeleteAsync(T entity)
         {
             //This should not be here when the local database is connected
@@ -61,6 +100,13 @@ namespace Packit.App.DataAccess
             return result.IsSuccessStatusCode;
         }
 
+        /// <summary>
+        /// update as an asynchronous operation.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns><c>true</c> if XXXX, <c>false</c> otherwise.</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
+        /// <exception cref="ArgumentNullException">entity</exception>
         public async Task<bool> UpdateAsync(T entity)
         {
             //This should not be here when the local database is connected
@@ -85,6 +131,11 @@ namespace Packit.App.DataAccess
             return result.IsSuccessStatusCode;
         }
 
+        /// <summary>
+        /// get all as an asynchronous operation.
+        /// </summary>
+        /// <returns>T[].</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
         public async Task<T[]> GetAllAsync()
         {
             //This should not be here when the local database is connected
@@ -100,6 +151,11 @@ namespace Packit.App.DataAccess
             return entities;
         }
 
+        /// <summary>
+        /// get all with child entities as an asynchronous operation.
+        /// </summary>
+        /// <returns>T[].</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
         public async Task<T[]> GetAllWithChildEntitiesAsync()
         {
             //This should not be here when the local database is connected
@@ -116,6 +172,12 @@ namespace Packit.App.DataAccess
             return entities;
         }
 
+        /// <summary>
+        /// get by identifier as an asynchronous operation.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>T.</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
         public async Task<T> GetByIdAsync(T entity)
         {
             //This should not be here when the local database is connected
@@ -132,6 +194,12 @@ namespace Packit.App.DataAccess
             return outEntity;
         }
 
+        /// <summary>
+        /// get by identifier with child entities as an asynchronous operation.
+        /// </summary>
+        /// <param name="entity">The entity.</param>
+        /// <returns>T.</returns>
+        /// <exception cref="NetworkConnectionException"></exception>
         public async Task<T> GetByIdWithChildEntitiesAsync(T entity)
         {
             //This should not be here when the local database is connected
--- Packit.App/ViewModels/ItemsViewModel.cs
+++ Packit.App/ViewModels/ItemsViewModel.cs
@@ -13,28 +13,89 @@ using Packit.App.Helpers;
 
 namespace Packit.App.ViewModels
 {
+    /// <summary>
+    /// Class ItemsViewModel.
+    /// Implements the <see cref="Packit.App.ViewModels.ViewModel" />
+    /// </summary>
+    /// <seealso cref="Packit.App.ViewModels.ViewModel" />
     public class ItemsViewModel : ViewModel
     {
+        #region private fields
+        /// <summary>
+        /// The items data access
+        /// </summary>
         private readonly IBasicDataAccess<Item> itemsDataAccess = new BasicDataAccessFactory<Item>().Create();
+        /// <summary>
+        /// The loaded command
+        /// </summary>
         private ICommand loadedCommand;
+        /// <summary>
+        /// The images data access
+        /// </summary>
         private readonly ImagesDataAccess imagesDataAccess = new ImagesDataAccess();
+        /// <summary>
+        /// The is visible
+        /// </summary>
         private bool isVisible;
+        /// <summary>
+        /// The item clone
+        /// </summary>
         private Item itemClone;
+        #endregion
 
+        #region public properties
+        /// <summary>
+        /// Gets or sets a value indicating whether this instance is visible.
+        /// </summary>
+        /// <value><c>true</c> if this instance is visible; otherwise, <c>false</c>.</value>
         public bool IsVisible
         {
             get => isVisible;
             set => Set(ref isVisible, value);
         }
 
+        /// <summary>
+        /// Gets the loaded command.
+        /// </summary>
+        /// <value>The loaded command.</value>
         public virtual ICommand LoadedCommand => loadedCommand ?? (loadedCommand = new NetworkErrorHandlingRelayCommand<ItemsPage>(async () => await LoadDataAsync(), PopUpService));
+        /// <summary>
+        /// Gets or sets the edit command.
+        /// </summary>
+        /// <value>The edit command.</value>
         public ICommand EditCommand { get; set; }
+        /// <summary>
+        /// Gets or sets the delete command.
+        /// </summary>
+        /// <value>The delete command.</value>
         public ICommand DeleteCommand { get; set; }
+        /// <summary>
+        /// Gets or sets the add command.
+        /// </summary>
+        /// <value>The add command.</value>
         public ICommand AddCommand { get; set; }
+        /// <summary>
+        /// Gets or sets the item to edit command.
+        /// </summary>
+        /// <value>The item to edit command.</value>
         public ICommand ItemToEditCommand { get; set; }
+        /// <summary>
+        /// Gets or sets the item done editing command.
+        /// </summary>
+        /// <value>The item done editing command.</value>
         public ICommand ItemDoneEditingCommand { get; set; }
+        /// <summary>
+        /// Gets the item image links.
+        /// </summary>
+        /// <value>The item image links.</value>
         public ObservableCollection<ItemImageLink> ItemImageLinks { get; } = new ObservableCollection<ItemImageLink>();
+        #endregion
 
+        #region constructors
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ItemsViewModel"/> class.
+        /// </summary>
+        /// <param name="popUpService">The pop up service.</param>
         public ItemsViewModel(IPopUpService popUpService)
             : base(popUpService)
         {
@@ -54,13 +115,45 @@ namespace Packit.App.ViewModels
                await UpdateEditeditem(param);
             }, popUpService);
         }
+        #endregion
 
+        #region private loading methods
+        /// <summary>
+        /// load data as an asynchronous operation.
+        /// </summary>
         private async Task LoadDataAsync()
         {
             await LoadItemsAsync();
             await LoadImagesAsync();
         }
 
+
+        /// <summary>
+        /// load items as an asynchronous operation.
+        /// </summary>
+        protected async Task LoadItemsAsync()
+        {
+            var items = await itemsDataAccess.GetAllAsync();
+
+            foreach (var i in items)
+                ItemImageLinks.Add(new ItemImageLink() { Item = i });
+        }
+
+        /// <summary>
+        /// load images as an asynchronous operation.
+        /// </summary>
+        protected async Task LoadImagesAsync()
+        {
+            foreach (var iml in ItemImageLinks)
+                iml.Image = await imagesDataAccess.GetImageAsync(iml.Item.ImageStringName, "ms-appx:///Assets/grey.jpg");
+        }
+        #endregion
+
+        #region private update methods
+        /// <summary>
+        /// Updates the editeditem.
+        /// </summary>
+        /// <param name="item">The item.</param>
         private async Task UpdateEditeditem(Item item)
         {
             if (StringIsEqual(item.Description, itemClone.Description) && StringIsEqual(item.Title, itemClone.Title))
@@ -73,7 +166,13 @@ namespace Packit.App.ViewModels
                 await PopUpService.ShowCouldNotSaveChangesAsync(itemClone.Title);
             }
         }
+        #endregion
 
+        #region private update methods
+        /// <summary>
+        /// delete item as an asynchronous operation.
+        /// </summary>
+        /// <param name="itemImageLink">The item image link.</param>
         private async Task DeleteItemAsync(ItemImageLink itemImageLink)
         {
             if (!string.IsNullOrEmpty(itemImageLink.Item.ImageStringName))
@@ -82,6 +181,10 @@ namespace Packit.App.ViewModels
                 await DeleteItemRequestAsync(itemImageLink);
         }
 
+        /// <summary>
+        /// delete item and image request as an asynchronous operation.
+        /// </summary>
+        /// <param name="itemImageLink">The item image link.</param>
         private async Task DeleteItemAndImageRequestAsync(ItemImageLink itemImageLink)
         {
             if (await itemsDataAccess.DeleteAsync(itemImageLink.Item) && await imagesDataAccess.DeleteImageAsync(itemImageLink.Item.ImageStringName))
@@ -90,6 +193,10 @@ namespace Packit.App.ViewModels
                 await PopUpService.ShowCouldNotDeleteAsync(itemImageLink.Item.Title);
         }
 
+        /// <summary>
+        /// delete item request as an asynchronous operation.
+        /// </summary>
+        /// <param name="itemImageLink">The item image link.</param>
         private async Task DeleteItemRequestAsync(ItemImageLink itemImageLink)
         {
             if (await itemsDataAccess.DeleteAsync(itemImageLink.Item))
@@ -97,19 +204,6 @@ namespace Packit.App.ViewModels
             else
                 await PopUpService.ShowCouldNotDeleteAsync(itemImageLink.Item.Title);
         }
-
-        protected async Task LoadItemsAsync()
-        {
-            var items = await itemsDataAccess.GetAllAsync();
-
-            foreach (var i in items)
-                ItemImageLinks.Add(new ItemImageLink() { Item = i });
-        }
-
-        protected async Task LoadImagesAsync()
-        {
-            foreach (var iml in ItemImageLinks)
-                iml.Image = await imagesDataAccess.GetImageAsync(iml.Item.ImageStringName, "ms-appx:///Assets/grey.jpg");
-        }
+        #endregion
     }
 }
